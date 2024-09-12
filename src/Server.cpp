bf5f0ade142dd78be89f75b9ed670fdd67237ab9
mrtea9 <ionmeriacri111@gmail.com>
2024-09-12 11:27:12 +0300

    combining character classes

--- src/Server.cpp
+++ src/Server.cpp
@@ -17,7 +17,7 @@ int matchhere(char* regexp, char* text) {
     if (regexp[0] == '\0') return 1;
     if (*regexp == ' ') return matchhere(regexp + 1, text);
     if (*regexp == '\\' && regexp[1] == 'd') return matchDigit(regexp + 2, text);
-    if (*regexp == '\\' && regexp[1] == 's') return matchLetter(regexp + 2, text);
+    if (*regexp == '\\' && regexp[1] == 'w') return matchLetter(regexp + 2, text);
     if (*text != '\0' && (regexp[0] == '.' || regexp[0] == *text)) {
         //std::cout << "avem" << std::endl;
         return matchhere(regexp + 1, text + 1);
