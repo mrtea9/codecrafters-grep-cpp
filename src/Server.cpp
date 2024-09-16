7ce813cfa0ec6d6f5d916ae9fbf5b4b3e25f0aa1
mrtea9 <ionmeriacri111@gmail.com>
2024-09-16 17:30:22 +0300

    combining character classes

--- src/Server.cpp
+++ src/Server.cpp
@@ -224,18 +224,22 @@ int capturedGroup(char* regexp, char* text) {
                 std::cout << "[captured Backreference Group] start pos: " << begin_group << std::endl;
                 std::cout << "[captured Backreference Group] end pos: " << end_group << std::endl;
 
+                std::cout << "[captured Backreference Group2] RegExp: " << string_regexp << std::endl;
+                std::cout << "[captured1]: " << captured << std::endl;
                 captured = string_regexp.substr(begin_group + 1, end_group - 1);
+                std::cout << "[captured2]: " << captured << std::endl;
+                std::cout << "[captured Backreference Group3] RegExp: " << string_regexp << std::endl;
 
                 string_regexp = ReplaceAll(string_regexp, reference, captured);
-                std::cout << "[captured Backreference Group2] RegExp: " << string_regexp << std::endl;
+                std::cout << "[captured Backreference Group4] RegExp: " << string_regexp << std::endl;
 
                 begin_group = string_regexp.find_first_of('(');
                 string_regexp.replace(begin_group, 1, "");
                 end_group = string_regexp.find_first_of(')');
                 string_regexp.replace(end_group, 1, "");
 
-                std::cout << "[captured Backreference Group3] RegExp: " << string_regexp << std::endl;
-                std::cout << "[captured]: " << captured << std::endl;
+                std::cout << "[captured Backreference Group5] RegExp: " << string_regexp << std::endl;
+                std::cout << "[captured3]: " << captured << std::endl;
             }
         }
 
