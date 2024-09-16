31eb18a6748e1bf6d45b7e8755fc1928d310d8fc
mrtea9 <ionmeriacri111@gmail.com>
2024-09-16 15:25:37 +0300

    combining character classes

--- src/Server.cpp
+++ src/Server.cpp
@@ -126,6 +126,8 @@ int matchOptional(char c, char* regexp, char* text) {
     std::cout << "[Optional] Text: " << text << std::endl;
     std::cout << "[Optional] RegExp: " << regexp << std::endl;
 
+    if (*text++ != '') return 0;
+
     return c == *text ? matchHere(regexp, text + 1) : matchHere(regexp, text);
 }
 
