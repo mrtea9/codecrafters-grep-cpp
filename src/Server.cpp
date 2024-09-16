42d45702ca971e9c67d37f652ff1049958486a5f
mrtea9 <ionmeriacri111@gmail.com>
2024-09-16 17:26:43 +0300

    combining character classes

--- src/Server.cpp
+++ src/Server.cpp
@@ -230,9 +230,8 @@ int capturedGroup(char* regexp, char* text) {
                 std::cout << "[captured Backreference Group2] RegExp: " << string_regexp << std::endl;
 
                 begin_group = string_regexp.find_first_of('(');
-                end_group = string_regexp.find_first_of(')');
-
                 string_regexp.replace(begin_group, 1, "");
+                end_group = string_regexp.find_first_of(')');
                 string_regexp.replace(end_group, 1, "");
 
                 std::cout << "[captured Backreference Group3] RegExp: " << string_regexp << std::endl;
