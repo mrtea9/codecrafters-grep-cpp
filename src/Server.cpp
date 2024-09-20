49cd52c4896bf8e58e80cabdc2cabe208f38a784
mrtea9 <ionmeriacri111@gmail.com>
2024-09-20 12:44:53 +0300

    Nested Backreferences

--- src/Server.cpp
+++ src/Server.cpp
@@ -392,10 +392,10 @@ int capturedGroup(char* regexp, char* text) {
                     result = captured_array;
                 }
 
-                //begin_group = string_regexp.find_first_of('(');
-                //string_regexp.replace(begin_group, 1, "");
-                //end_group = string_regexp.find_first_of(')');
-                //string_regexp.replace(end_group, 1, "");
+                begin_group = string_regexp.find_first_of('(');
+                string_regexp.replace(begin_group, 1, "");
+                end_group = string_regexp.find_first_of(')');
+                string_regexp.replace(end_group, 1, "");
 
                 //string_regexp = ReplaceAll(string_regexp, reference, result);
 
