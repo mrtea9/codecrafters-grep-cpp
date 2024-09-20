44fd942db34ec322d28ec4f702cc00ff08ed60eb
mrtea9 <ionmeriacri111@gmail.com>
2024-09-20 12:45:15 +0300

    Nested Backreferences

--- src/Server.cpp
+++ src/Server.cpp
@@ -397,7 +397,7 @@ int capturedGroup(char* regexp, char* text) {
                 end_group = string_regexp.find_first_of(')');
                 string_regexp.replace(end_group, 1, "");
 
-                //string_regexp = ReplaceAll(string_regexp, reference, result);
+                string_regexp = ReplaceAll(string_regexp, reference, result);
 
 
                 std::cout << "[captured Backreference Group] RegExp: " << string_regexp << std::endl;
