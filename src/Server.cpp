0eb980c246b16787309798ccc9dfac5079ecbb7b
mrtea9 <ionmeriacri111@gmail.com>
2024-09-24 15:43:09 +0300

    Nested Backreferences

--- src/Server.cpp
+++ src/Server.cpp
@@ -405,7 +405,6 @@ int capturedGroup(char* regexp, char* text) {
                         std::cout << "[string_regexp brace]: " << string_regexp << std::endl;
 
                         captured_groups.push_back(test);
-                        break;
                     }
 
                     if (string_regexp[pos_brace] == '\\' && string_regexp[pos_brace + 1] != 'w' && string_regexp[pos_brace + 1] != 'd') {
@@ -421,14 +420,16 @@ int capturedGroup(char* regexp, char* text) {
                         openBrace--;
 
                         captured_groups.push_back(test);
-                        break;
                     }
 
                     pos_brace++;
                     if (pos_brace == string_regexp.length()) break;
                 }
 
-                for (int i = 0; i < captured_groups.size(); i++) std::cout << captured_groups[i] << "\n";
+               
+                for (int i = 0; i < captured_groups.size(); i++) {
+                    std::cout << "[CAPTURED3]: " << captured_groups[i] << std::endl;
+                }
 
                 begin_group = string_regexp.find_first_of('(');
                 end_group = string_regexp.find_first_of(')');
