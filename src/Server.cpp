66faeddbba5032cd7e5997bd1f57570a832e3ea4
mrtea9 <ionmeriacri111@gmail.com>
2024-09-27 10:22:20 +0300

    Nested Backreferences

--- src/Server.cpp
+++ src/Server.cpp
@@ -74,7 +74,7 @@ int matchBackreference(char* regexp, char* orig_regexp, char* text) {
     std::cout << "[matchBackreference string_orig]: " << string_orig << std::endl;
     std::cout << "[matchBackreference result]: " << toChar(string_orig) << std::endl;
 
-    return matchHere(toChar(string_orig), text);
+    return match(toChar(string_orig), text);
 }
 
 int matchParentheses(char* regexp, char* orig_regexp, char* text) {
