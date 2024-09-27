3c96f69c67a13fbbf6723ce86bce0ca41a81a93d
mrtea9 <ionmeriacri111@gmail.com>
2024-09-27 10:02:28 +0300

    Nested Backreferences

--- src/Server.cpp
+++ src/Server.cpp
@@ -22,11 +22,12 @@ int matchBackreference(char reference, char* regexp, char* orig_regexp) {
     int len = 0;
     int openBrackets = 1;
     int closedBrackets = 0;
+    int count = 1;
     size_t start_pos = 0;
     std::string string_regexp = regexp;
     std::string string_orig = orig_regexp;
     std::string captured;
-    std::string reference_full = "\\" + std::string() + reference;
+    std::string reference_full = "\\" + std::string() + count;
     std::cout << "[matchBackreference orig_regexp]: " << string_orig << std::endl;
     start_pos = string_orig.find(string_regexp);
     
